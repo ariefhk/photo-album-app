c3da8b537e316662aed1b8bee5d228ffecc393cd
Arief Rachman Hakim <ariefrachmanhakim1001@gmail.com>
2024-03-14 00:02:37 +0700

    feat: upload image

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,16 +1,32 @@
-"use client";
+import CloudinaryImage from "./cloudinary-image";
+import UploadButton from "./upload-button";
+import cloudinary from "cloudinary";
 
-import { CldUploadButton } from "next-cloudinary";
-import { Button } from "@/components/ui/button";
+type SearchResult = {
+  public_id: string;
+};
 
-const GalleryPage = () => {
+const GalleryPage = async () => {
+  const results = (await cloudinary.v2.search
+    .expression("resource_type:image")
+    .sort_by("created_at", "desc")
+    .max_results(10)
+    .execute()) as { resources: SearchResult[] };
+
+  console.log(results);
+
   return (
     <section>
-      <div className="flex justify-between">
-        <h1 className="text-4xl font-bold">Gallery</h1>
-        <Button asChild>
-          <CldUploadButton uploadPreset="ru1pmy2k" />
-        </Button>
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-between">
+          <h1 className="text-4xl font-bold">Gallery</h1>
+          <UploadButton />
+        </div>
+        <div className="grid grid-cols-4 gap-4">
+          {results.resources.map((result, idx) => {
+            return <CloudinaryImage key={idx + 1} src={result.public_id} alt="Image of something" width="400" height="300" />;
+          })}
+        </div>
       </div>
     </section>
   );
---
+++ src/app/gallery/upload-button.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { CldUploadButton } from "next-cloudinary";
+import { useRouter } from "next/navigation";
+
+const UploadButton = () => {
+  const router = useRouter();
+
+  return (
+    <Button asChild>
+      <CldUploadButton
+        uploadPreset="ru1pmy2k"
+        onSuccess={() => {
+          console.log("refresh");
+          setTimeout(() => {
+            router.refresh();
+          }, 1000);
+        }}
+      />
+    </Button>
+  );
+};
+
+export default UploadButton;
